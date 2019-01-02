729d9c436fc4b4a6151ffa3b58afb696a5c26834
David Hiller <HillerDavid@users.noreply.github.com>
2019-01-01 20:11:58 -0500

    Initial commit

---
+++ models/Article.js
@@ -0,0 +1,35 @@
+let mongoose = require('mongoose')
+
+let Schema = mongoose.Schema
+
+let ArticleSchema = new Schema({
+    headline: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    summary: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    URL: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    score: {
+        type: Number,
+        required: true
+    },
+    notes: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Note"
+        }
+    ]
+})
+
+let Article = mongoose.model('Article', ArticleSchema)
+
+module.exports = Article
