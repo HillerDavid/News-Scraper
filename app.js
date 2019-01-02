5474d01c301a12dd7ed2b68aa6e849992ba8fa3a
David Hiller <HillerDavid@users.noreply.github.com>
2019-01-02 13:24:08 -0500

    adjusted to using express-handlebars, begun adding styling

--- app.js
+++ app.js
@@ -1,34 +1,44 @@
 var createError = require('http-errors');
 var express = require('express');
+let exphbs = require('express-handlebars')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
 
-var indexRouter = require('./controllers/routes/index');
-var usersRouter = require('./controllers/routes/users');
+var htmlRoutes = require('./controllers/routes/htmlRoutes');
+var apiRoutes = require('./controllers/routes/apiRoutes');
 
 var app = express();
 
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/scraper_db'
+
+var db = require('./models')
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  partialsDir: __dirname + '/views/partials'
+}))
+app.set('view engine', 'handlebars');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
